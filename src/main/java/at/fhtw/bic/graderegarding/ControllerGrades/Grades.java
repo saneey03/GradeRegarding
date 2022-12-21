4fa5db71fd24b060535f2048fe2ffc4ce182655d
Sanja Ilic <ic21b110@technikum-wien.at>
2022-12-21 18:47:33 +0100

    Initial commit

--- src/main/java/at/fhtw/bic/graderegarding/ControllerGrades/Grades.java
+++ src/main/java/at/fhtw/bic/graderegarding/ControllerGrades/Grades.java
@@ -4,14 +4,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Scanner;
+
 @RestController
 public class Grades {
     @GetMapping("/Grades")
-    public double GradeRegarding(@RequestParam double relative){
-        //double GradeOne = 90% = 1;
-        //double GradeTwo = 50% = 4;
-        //double GradeThree = 0% = 5;
-        return 0;
-    }
+    public static void main(String args[])
+    {
+        int marks[] = new int[6];
+        int i;
+        float total=0, avg;
+        Scanner scanner = new Scanner(System.in);
 
+
+        for(i=0; i<6; i++) {
+            System.out.print("Enter Marks of Subject"+(i+1)+":");
+            marks[i] = scanner.nextInt();
+            total = total + marks[i];
+        }
+        scanner.close();
+        //Calculating average here
+        avg = total/6;
+        System.out.print("The student Grade is: ");
+        if(avg>=80)
+        {
+            System.out.print("1");
+        }
+        else if(avg>=60 && avg<80)
+        {
+            System.out.print("2");
+        }
+        else if(avg>=40 && avg<60)
+        {
+            System.out.print("3");
+        }
+        else
+        {
+            System.out.print("4");
+        }
+    }
 }
