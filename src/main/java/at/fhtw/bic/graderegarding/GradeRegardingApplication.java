4c9dafcc096b407a7c643b3245bab103143821bb
Sanja Ilic <ic21b110@technikum-wien.at>
2022-12-21 18:04:18 +0100

    Initial commit

---
+++ src/main/java/at/fhtw/bic/graderegarding/GradeRegardingApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.graderegarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeRegardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeRegardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhtw/bic/graderegarding/GradeRegardingApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.graderegarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeRegardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
